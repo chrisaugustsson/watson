ca04b32f48037ef90cbf21e8961ba94b49501861
Christopher Augustsson <chris.augustsson@live.se>
2020-12-18 11:16:59 +0100

    First commit

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const watson = async () => {
+  const assistant = new AssistantV2({
+    version: '2020-12-18',
+    authenticator: new IamAuthenticator({
+      apikey: 'gVDCIAZw86546Uk7G_mnfm33MPNV44iUuTEVAR6jsWnr',
+    }),
+    serviceUrl: 'https://api.eu-de.assistant.watson.cloud.ibm.com/instances/e9b8c16b-3b5a-42be-bce9-5047e4661c7f',
+  });
+  
+  let sessionId = await assistant.createSession({assistantId: 'a9244c0b-4b99-42a5-ad22-338926eb33eb'});
+
+  assistant.message({
+  assistantId: 'a9244c0b-4b99-42a5-ad22-338926eb33eb',
+  sessionId: sessionId.result.session_id,
+  input: {
+    'message_type': 'text',
+    'text': 'Hello'
+    }
+  })
+  .then(res => {
+    console.log(JSON.stringify(res.result, null, 2));
+  })
+  .catch(err => {
+    console.log(err);
+  });
+}
+
+watson()
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+
+
---
+++ services/watson.js
@@ -0,0 +1,11 @@
+// const AssistantV2 = require('ibm-watson/assistant/v2');
+// const { IamAuthenticator } = require('ibm-watson/auth');
+
+// const assistant = new AssistantV2({
+//   version: '{version}',
+//   authenticator: new IamAuthenticator({
+//     apikey: '{apikey}',
+//   }),
+//   serviceUrl: '{url}',
+// });
+
